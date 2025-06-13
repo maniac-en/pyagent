f9f36fc59393fabebdee721b0c1d15a8639ab7d8
Shivam Mehta <sm.cse17@gmail.com>
2025-06-14 01:58:39 +0530

    ch1_LLMs: L6_Verbose

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import os
 import sys
+from typing import Type
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+api_key: [str | None] = os.environ.get("GEMINI_API_KEY")
+client: Type[genai.Client] = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 2:
-    print("Input prompt not provided")
+user_prompt: str = None
+is_verbose: bool = False
+
+if (len(sys.argv) < 2) or (sys.argv[-1] == "--verbose" and len(sys.argv) < 3):
+    print("input prompt not provided")
     sys.exit(1)
-prompt = sys.argv[1:]
+elif sys.argv[-1] == "--verbose":
+    is_verbose = True
+    user_prompt = str(sys.argv[1:-1])
+else:
+    user_prompt = str(sys.argv[1:])
+
+if user_prompt is None:
+    print("something went wrong, check your stupid code")
+    sys.exit(1)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt,
+    contents=messages,
 )
+
 print(response.text)
-print(
-    "Prompt tokens: {}\nResponse tokes: {}".format(
-        response.usage_metadata.prompt_token_count,
-        response.usage_metadata.candidates_token_count,
-    )
-)
+if is_verbose:
+    print(f"""\
+User prompt: {user_prompt}
+Prompt tokens: {response.usage_metadata.prompt_token_count}
+Response tokens: {response.usage_metadata.candidates_token_count}\
+        """)
