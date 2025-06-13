c1c4bd897ba740cb874f81e5396467f7e98b3a0d
Shivam Mehta <sm.cse17@gmail.com>
2025-06-14 01:31:40 +0530

    ch1_LLMs: L4_Input

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,9 +8,14 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+if len(sys.argv) < 2:
+    print("Input prompt not provided")
+    sys.exit(1)
+prompt = sys.argv[1:]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."',
+    contents=prompt,
 )
 print(response.text)
 print(
