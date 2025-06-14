8e691bf39b57925d20dbf60f45fee5572c488863
Shivam Mehta <sm.cse17@gmail.com>
2025-06-15 02:49:09 +0530

    CH3_Function_Calling: L1_System_Prompt

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+
+system_prompt: str = (
+    'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+)
+
 load_dotenv()
 api_key: [str | None] = os.environ.get("GEMINI_API_KEY")
 client: Type[genai.Client] = genai.Client(api_key=api_key)
@@ -33,6 +38,7 @@ messages = [
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 print(response.text)
